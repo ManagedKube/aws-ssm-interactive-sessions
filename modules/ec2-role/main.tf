e5251deabfff84188cff80b0ac1eb53debf0e928
Garland Kan <garlandk@gmail.com>
2025-05-01 11:46:24 -0700

    Update main.tf

--- modules/ec2-role/main.tf
+++ modules/ec2-role/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "s3" {
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_iam_instance_profile" "test_profile" {
   name = "OpsEC2SSM${var.name}"
   role = aws_iam_role.role.name
