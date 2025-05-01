2cd97f1fa10bcf68a23682f255702aeefa185c8c
Garland Kan <garlandk@gmail.com>
2025-05-01 14:09:26 -0700

    Update main.tf

--- modules/user-policies/attach-policy-to-role/main.tf
+++ modules/user-policies/attach-policy-to-role/main.tf
@@ -2,5 +2,4 @@ resource "aws_iam_role_policy_attachment" "attach" {
   count      = length(var.role_list)
   role      = var.role_list[count.index]
   policy_arn = var.policy_arn
-  policy_arn = aws_iam_policy.policy.arn
 }
